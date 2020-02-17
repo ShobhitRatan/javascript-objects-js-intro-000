3d2d6b8ee53d31c650e3d5769fad6a16f184d92a
Shobhit Ratan <shobhit.ratan@baruchmail.cuny.edu>
2020-02-17 02:57:52 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,1 +1,8 @@
 var playlist = {artistName: "songTitle"};
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist['Slowdive'] = 'song';
+  playlist['My Bloody Valentine'] = 'song2';
+  playlist['Phil Ochs'] = 'song3';
+  return playlist;
+}
